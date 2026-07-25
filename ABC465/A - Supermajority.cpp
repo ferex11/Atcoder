8b2e81b534d293842330c507ecbe5eb47cfa2696
Sujeet Kumar <sujeet901626@gmail.com>
2026-07-25 18:56:30 +0530

    Create A - Supermajority.cpp

---
+++ ABC465/A - Supermajority.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  if (3 * a > 2 * b) cout << "Yes\n";
+  else cout << "No\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  solve();
+  return 0;
+}
