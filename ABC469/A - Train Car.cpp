3c825091cab6841b0b6973559ee4b5565ff08674
Sujeet Kumar <sujeet901626@gmail.com>
2026-08-02 09:08:54 +0530

    Create A - Train Car.cpp

---
+++ ABC469/A - Train Car.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+  cout << n - k + 1 << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  solve();
+  return 0;
+}
